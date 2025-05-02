f93b96a9057b9051bb6fb4375e816de70936abe2
balo <balo@alexandria.pc>
2025-05-02 18:38:13 +0100

    Hot fix

--- live.sh
+++ live.sh
@@ -1,21 +1,22 @@
 
 echo '===========================Updating os and installing dependencies================================'
 sudo apt update
-sudo apt install -y python3-pip unzip xvfb \
+sudo apt install -y python3-pip unzip \
     libxi6 libgconf-2-4 libnss3 libxss1 libappindicator1 \
     fonts-liberation libatk-bridge2.0-0 libgtk-3-0
+sudo apt install xvfb
 sudo curl -LsSf https://astral.sh/uv/install.sh | sh
 echo '=========================Update completed================================'
 
 echo '===========================Installing Chrome================================'
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 echo '=========================Chrome installation completed================================'
 
 echo '===========================Installing ChromeDriver================================'
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
-wget https://chromedriver.storage.googleapis.com/${CHROME_VERSION}.0.0/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
+sudo wget https://chromedriver.storage.googleapis.com/${CHROME_VERSION}.0.7103.49/chromedriver_linux64.zip
+sudo unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
 echo '=========================ChromeDriver installation completed================================'
