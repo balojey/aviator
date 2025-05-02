1c90e98cb9683a6e539fc3d1ef0012cc3ee022d4
balo <balo@alexandria.pc>
2025-05-02 18:57:38 +0100

    Hot fix

--- live.sh
+++ live.sh
@@ -10,7 +10,7 @@ sudo apt install -y libappindicator1
 sudo apt install -y fonts-liberation
 sudo apt install -y libatk-bridge2.0-0
 sudo apt install -y libgtk-3-0
-sudo apt install -y xvfb 
+sudo apt install -y xvfb
 sudo apt install -y unzip
 sudo curl -LsSf https://astral.sh/uv/install.sh | sh
 echo '=========================Update completed================================'
@@ -23,7 +23,7 @@ echo '=========================Chrome installation completed====================
 echo '===========================Installing ChromeDriver================================'
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
 sudo wget https://storage.googleapis.com/chrome-for-testing-public/${CHROME_VERSION}.0.7103.49/linux64/chromedriver-linux64.zip
-sudo unzip chromedriver_linux64.zip
+sudo unzip chromedriver-linux64.zip
 sudo mv chromedriver /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
 echo '=========================ChromeDriver installation completed================================'
