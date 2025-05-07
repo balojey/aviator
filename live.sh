c7a20526d5f5ff883bced0d08255ccf44a3b2822
balo <balo@alexandria.pc>
2025-05-07 17:01:32 +0100

    Hot fix

--- live.sh
+++ live.sh
@@ -43,5 +43,5 @@ echo '=========================Project dependencies installation completed======
 
 echo '===========================Running script================================'
 mkdir -p logs/live/
-uv run eagle_shot.py test
+uv run loss_lurkey.py test
 echo '=========================Script execution completed================================'
